2dd9bc83a6ad0723d570d00de63edbf3a6c362fd
Priyabrat Personal <dipujkbk@gmail.com>
2025-07-05 20:14:08 +0530

    Quantity selection

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
         </div>
 
         <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
             <option selected value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -72,11 +72,13 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
         */
     const productId = button.dataset.productId;
 
+    let productQuantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
+
     let matchingProductFound = false;
 
     cart.forEach((item) => {
       if (productId === item.productId) {
-        item.quantity++;
+        item.quantity += productQuantity;
         matchingProductFound = true;
       }
     });
@@ -84,7 +86,7 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
     if (!matchingProductFound) {
       cart.push({
         productId,
-        quantity: 1,
+        quantity: productQuantity,
       });
     }
 
