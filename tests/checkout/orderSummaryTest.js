816aa849375d06ea5ab0889cf39b35b95d7280b4
Priyabrat Personal <dipujkbk@gmail.com>
2025-08-30 12:04:27 +0530

    loadProducts in beforeAll hook

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 import { loadFromStorage, cart } from "../../data/cart.js";
+import { loadProducts } from "../../data/products.js";
 
 describe("testsuite: renderOrderSummary", () => {
   /**
@@ -10,6 +11,12 @@ describe("testsuite: renderOrderSummary", () => {
 
   const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
   const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
+
+  beforeAll((done)=>{
+    loadProducts(()=>{
+      done(); //--> lets us control when to go to the next step
+    })
+  })
 
   beforeEach(() => {
     spyOn(localStorage, "setItem");
